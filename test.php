0bec5d7175df4915ea2e8dfd86e57756e39a150a
amjoey <amjoey@gmail.com>
2019-08-28 13:28:29 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,6 +1,11 @@
-<?php // callback.php
+<?php
 require "vendor/autoload.php";
-require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 $access_token = 'YuKnhNGPOcEMaAuYgEBR8sSs0AZZ8olN6D/c5PcOnNowDelexUmHE4C2Dzx0CpD0Ryvcul199PRzqTjTNP+NwaQXqepOrX+bpxWtz6X+y9LxgL3Gc9l8hZKjo+87hvi3EUzRfu5QnMULdVnTKnjFFQdB04t89/1O/w1cDnyilFU=';
-// Get POST body content
-file_put_contents('log.txt', file_get_contents('php://input') . PHP_EOL, FILE_APPEND);
+$channelSecret = '5112d54c06c00f9772bc951239d17fff';
+$idPush = 'U6df020ea314604da9aa481fdd0054c96'
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
+$response = $bot->pushMessage($idPush, $textMessageBuilder);
+
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
