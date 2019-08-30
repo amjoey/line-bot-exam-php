95574f18a2fc7104ce67a9fb18202da0d16208f9
amjoey <amjoey@gmail.com>
2019-08-30 08:40:27 +0700

    Create notify.php

---
+++ notify.php
@@ -0,0 +1,35 @@
+<?php
+
+define('ACCESS_TOKEN', 'P0ku2DjIlLItSPfQhCOtNj0OJpEA7qOLH2YfaC...');
+define('LINE_API_URI', 'https://notify-api.line.me/api/notify');
+
+$headers = [
+    'Authorization: Bearer ' . ACCESS_TOKEN
+];
+$fields = [
+    'message' => 'Your order #12345 has been delivered'
+];
+
+try {
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_URL, LINE_API_URI);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_POST, count($fields));
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+    $res = curl_exec($ch);
+    curl_close($ch);
+
+    if ($res == false)
+        throw new Exception(curl_error($ch), curl_errno($ch));
+
+    $json = json_decode($res);
+    $status = $json->status;
+
+    var_dump($status);
+} catch (Exception $e) {
+    var_dump($e);
+}
